d1cc578e32ee4ffc71eb7a7593fb63be29f24a43
Hla Thue Prue Marma <hlathuepruemarma@Hlas-MacBook-Air.local>
2026-04-19 03:45:28 +0300

    add to gane Details page

--- routs/Routs.jsx
+++ routs/Routs.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
 import { createBrowserRouter } from "react-router";
-import HomeLayouts from '../layouts/HomeLayouts';
-import Home from '../pages/Home';
-import GameCard from '../component/GameCard/GameCard';
-import GameStore from '../component/GameStore/GameStore';
-
+import HomeLayouts from "../layouts/HomeLayouts";
+import Home from "../pages/Home";
+import GameCard from "../component/GameCard/GameCard";
+import GameStore from "../component/GameStore/GameStore";
+import AuthLayouts from "../layouts/AuthLayouts";
+import Login from "../component/Login/Login";
+import Registration from "../component/Registration/Registration";
+import Myprofile from "../pages/My Profile/Myprofile";
+import GameDetails from "../component/Game Details/GameDetails";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayouts></HomeLayouts>,
-      children: [
-      { index: true, Component: Home },
-      { path: "gameCard", Component: GameCard },
-    ]
+    children: [{ index: true, Component: Home }],
+  },
+  {
+    path: "authLayouts",
+    element: <AuthLayouts></AuthLayouts>,
+    children: [
+      { path: "logIn", Component: Login },
+      { path: "registration", Component: Registration },
+    ],
+  },
+  {
+    path: "myProfile",
+    Component: Myprofile,
+  },
+  {
+    path: "gameDetails/:id",
+    loader: () => fetch("/game.json"),
+    Component: GameDetails,
   },
 ]);
